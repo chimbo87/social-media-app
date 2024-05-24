242810eca6b9007706a9f1c235474d23e8627566
chimbo87 <archfordnhanga@gmail.com>
2024-05-24 14:34:52 +0200

    designs

--- frontend/src/hooks/usePreviewimg.js
+++ frontend/src/hooks/usePreviewimg.js
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useState } from "react";
+import useShowToast from "./useShowToast";
 
 function usePreviewimg() {
-
-    const [imgUrl, setImgUrl] = useState(null);
-
-    const handleImageChange = (e)=>{
-        const file = e.target.files[0];
-        console.log(file)
+  const [imgUrl, setImgUrl] = useState(null);
+  const showToast = useShowToast();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file && file.type.startsWith("image/")) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImgUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      showToast("Invalid file type", "Please select an image file", "error");
+      setImgUrl(null);
     }
-  return {handleImageChange, imgUrl}
+  };
+  console.log(imgUrl);
+  return { handleImageChange, imgUrl };
 }
 
-export default usePreviewimg
+export default usePreviewimg;
