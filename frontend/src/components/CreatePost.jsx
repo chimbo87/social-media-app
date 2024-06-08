96314a633f7440fb809e77713c45c54168379e43
chimbo87 <archfordnhanga@gmail.com>
2024-06-08 14:09:03 +0200

    commit all

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,17 +1,136 @@
-import { Button, useColorModeValue } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useColorModeValue,
+  useDisclosure,
+  Text,
+  Input,
+  Flex,
+  CloseButton,
+  Image,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { GoPlus } from "react-icons/go";
+import usePreviewImg from "../hooks/usePreviewing";
+import { TiImageOutline } from "react-icons/ti";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/UserAtom";
+import useShowToast from "../hooks/useShowToast";
 
+const MAX_CHAR = 500;
 function CreatePost() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [postText, setPostText] = useState("");
+  const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
+  const imageRef = useRef(null);
+  const [remainingChar, setRemainingChar] = useState(MAX_CHAR);
+  const user = useRecoilValue(userAtom);
+  const showToast = useShowToast();
+
+  const handleTextChange = (e) => {
+    const inputText = e.target.value;
+
+    if (inputText.length > MAX_CHAR) {
+      const truncatedText = inputText.slice(0, MAX_CHAR);
+      setPostText(truncatedText);
+      setRemainingChar(0);
+    } else {
+      setPostText(inputText);
+      setRemainingChar(MAX_CHAR - inputText.length);
+    }
+  };
+  const handleCreatePost = async () => {
+    const res = await fetch("/api/posts/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ postedBy: user._id, text: postText, img: imgUrl }),
+    })
+    const data = await res.json()
+    if(data.error){
+      showToast("Error",data.error,"error")
+      return
+    }
+    showToast("Success", "Post created successfully", "success")
+    onClose()
+  };
+
   return (
     <>
       <Button
         position={"fixed"}
         bottom={10}
         right={10}
-        bg={useColorModeValue("gray.300", "gray.dark")}
+        bg={useColorModeValue("gray.300", "gray.700")}
+        onClick={onOpen}
       >
-        <GoPlus />  Post
+        <GoPlus /> Post
       </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <Textarea
+                placeholder="Post content goes here..."
+                onChange={handleTextChange}
+                value={postText}
+              />
+              <Text
+                fontSize="xs"
+                fontWeight="bold"
+                textAlign={"right"}
+                m={"1"}
+                color={"gray.800"}
+              >
+                {remainingChar}/{MAX_CHAR}
+              </Text>
+              <Input
+                type="file"
+                hidden
+                ref={imageRef}
+                onChange={handleImageChange}
+              />
+              <TiImageOutline
+                style={{ marginLeft: "5px", cursor: "pointer" }}
+                size={30}
+                onClick={() => imageRef.current.click()}
+              />
+            </FormControl>
+            {imgUrl && (
+              <Flex mt={5} w={"full"} position={"relative"}>
+                <Image src={imgUrl} alt="Selected img" />
+                <CloseButton
+                  onClick={() => {
+                    setImgUrl(null);
+                  }}
+                  bg={"gray.800"}
+                  position={"absolute"}
+                  top={2}
+                  right={2}
+                />
+              </Flex>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleCreatePost}>
+              Post
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
--- frontend/src/hooks/usePreviewing.js
+++ frontend/src/hooks/usePreviewing.js
@@ -20,7 +20,7 @@ function usePreviewing() {
     }
   };
 
-  return { handleImageChange, imgUrl };
+  return { handleImageChange, imgUrl, setImgUrl};
 }
 
 export default usePreviewing;
