a7c243ba040ad87a5502a50fb31fac3e4eeb0574
chimbo87 <archfordnhanga@gmail.com>
2024-06-27 16:32:11 +0200

    commit all

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import Message from "../models/messageModel.js"
 
 const app = express();
 const server = http.createServer(app);
@@ -15,11 +16,22 @@ export const getRecipientSocketId = (recipientId)=>{
     return  userSocketMap[recipientId];
 }
 const userSocketMap = {};
+
 io.on("connection", (socket) => {
   console.log("user connected", socket.id);
   const userId = socket.handshake.query.userId;
   if (userId != "undefined") userSocketMap[userId] = socket.id;
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("markMessagesAsSeen",async({conversationId,userId})=>{
+    try{
+await Message.updateMany({conversationId:conversationId,seen:false}, {$set:{seen:true}})
+await Conversation.updateOne({_id:conversationId},{$set:{"lastMessage.seen":true}})
+io.to(userSocketMap[userId]).emit("messageSeen", {conversationId});
+    }catch(error){
+      console.log(error)
+    }
+  })
 
   socket.on("disconnect", () => {
     delete userSocketMap[userId];
