1dfc53e570c2290e96cde2fc3ad75950cb3e5d0b
Archford Nhanga <archfordnhanga@gmail.com>
2024-08-15 23:24:59 +0200

    commit all

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -16,9 +16,12 @@ import {
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { useSetRecoilState } from "recoil";
-import authScreenAtom from "../atoms/authAtom";
 import useShowToast from "../hooks/useShowToast";
 
+const authScreenAtom = atom({
+	key: "authScreenAtom",
+	default: "login",
+});
 
 const userAtom = atom({
 	key: "userAtom",
